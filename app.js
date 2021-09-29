361f7696914dafa31a0f2b7d8f6fa560d5e865ed
EricLouteiro <ericlouteiro@gmail.com>
2021-09-29 18:18:04 -0400

    webserver commit

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
 
-const Server = require('./models/server');
+const port = process.env.PORT;
+const app = express();
 
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+// Servir contenido estatico
 
+app.use(express.static('public'));
 
-const server = new Server;
+app.get('/', (req, res)=>{
+    res.render('home', {
+        nombre: 'EricLouteiro',
+        titulo: 'App NodeJs'
+    });
+});
 
+app.get('/generic', (req, res)=>{
+    res.render('generic', {
+        nombre: 'EricLouteiro',
+        titulo: 'App NodeJs'
+    });
+});
+app.get('/elements', (req, res)=>{
+    res.render('elements', {
+        nombre: 'EricLouteiro',
+        titulo: 'App NodeJs'
+    });
+});
+app.get('/hola-mundo', (req, res)=>{
+    res.send('holamundo en su respectiva ruta');
+});
 
-server.listen();
+app.get('*', (req, res)=>{
+    res.send('404 | Page not found');
+});
+
+app.listen(port, ()=>{
+    console.log(`app listenning on port: ${port}`)
+});
